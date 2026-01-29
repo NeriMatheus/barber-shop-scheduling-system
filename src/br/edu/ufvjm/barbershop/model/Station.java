46b1b52729537974f50c00d41edfcc07a11a3b35
Matheus Neri <matheus.man.mm@gmail.com>
2026-01-28 23:21:15 -0300

    .

---
+++ src/br/edu/ufvjm/barbershop/model/Station.java
@@ -0,0 +1,11 @@
+package br.edu.ufvjm.barbershop.model;
+
+public class Station {
+
+    private int number;
+    private String description;
+    private Employee responsable;
+    private boolean ocuppied;
+
+    
+}
